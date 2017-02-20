1ded9faf46f60855d40f564cd2c4e8d65e288a36
Emanuela <dookie@live.it>
2017-02-20 12:13:52 +0200

    w5 fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,13 @@ Rails.application.routes.draw do
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
+  resources :places, only:[:index, :show]
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'  
   root 'breweries#index'
   get 'places', to: 'places#index'
+  # get 'places/:id', to:'places#show'
   post 'places', to:'places#search'
+  get 'panimo/:id', to: 'breweries#show'
 end
